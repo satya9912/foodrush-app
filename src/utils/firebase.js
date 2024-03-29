78ec716f41a158319288461c1c4e1cad23c86ad0
satya9912 <satyakarri22@gmail.com>
2024-03-29 14:41:36 +0530

    fixed error page

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAaEw7pUKks0H3I03WgAv--WTEbzr4LJzk",
-  authDomain: "foodrush-app-39465.firebaseapp.com",
-  projectId: "foodrush-app-39465",
-  storageBucket: "foodrush-app-39465.appspot.com",
-  messagingSenderId: "252051634006",
-  appId: "1:252051634006:web:daef5221f49a4f763dc8f7",
-  measurementId: "G-MD89FF21ZE"
+  apiKey: "AIzaSyA3baXil-EzIvr01aMMhXLNgyffC2SEZD0",
+  authDomain: "foodrush-55dba.firebaseapp.com",
+  projectId: "foodrush-55dba",
+  storageBucket: "foodrush-55dba.appspot.com",
+  messagingSenderId: "630928435670",
+  appId: "1:630928435670:web:8d6fcf7a8f888c8a3f9c77",
+  measurementId: "G-5SYVBB4JZE"
 };
 
 // Initialize Firebase
